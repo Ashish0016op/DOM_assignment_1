064ed6bfaee05427c4820128bbf8a4ac0ffbede6
Ashish0016op <raccoonop0016@gmail.com>
2023-05-01 14:14:14 +0530

    tagName commit

--- dom.js
+++ dom.js
@@ -1,5 +1,13 @@
-let items=document.getElementsByClassName('list-group-item');
-items[2].style.backgroundColor='green';
-for(let i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold';
+//getElementByClassName
+// let items=document.getElementsByClassName('list-group-item');
+// items[2].style.backgroundColor='green';
+// for(let i=0;i<items.length;i++){
+//     items[i].style.fontWeight='bold';
+// }
+//getElementBytagName
+let li=document.getElementsByTagName('li');
+li[2].textContent='Hello2';
+li[2].style.backgroundColor='green';
+for(let i=0;i<li.length;i++){
+    li[i].style.fontWeight='bold';
 }
