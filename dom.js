14e37f238a0566d5ea697e418695494b4445563c
Ashish0016op <raccoonop0016@gmail.com>
2023-05-01 13:57:14 +0530

    commit_items

--- dom.js
+++ dom.js
@@ -1,2 +1,5 @@
-let headerTitle=document.getElementById('header-title');
-console.log(headerTitle.textContent);
+let items=document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor='green';
+for(let i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
